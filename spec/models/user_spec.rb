d204e2b68b818f5d0bc7ea9682d76802d92eb916
Vitalya Shevtsov <kaktusyaka@gmail.com>
2013-06-20 21:25:03 +0300

    added rspec to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,16 @@ describe User do
   end
 
   context "validations" do
+    it { should validate_confirmation_of(:password) }
+    it { should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:email) }
+
     it { should_not allow_value('asd', 'asd@oit', 'jahs@ljkhsd.com').for(:email) }
     it { should allow_value(Faker::Internet.email).for(:email) }
+
+    it { should allow_mass_assignment_of(:email) }
+    it { should allow_mass_assignment_of(:password) }
+    it { should allow_mass_assignment_of(:password_confirmation) }
   end
 
   context "Methods" do
